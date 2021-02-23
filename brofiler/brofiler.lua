b3033c94b886da75c486ce51f144e527bdedbe18
DESKTOP-F4OSCOB\Alex-desktop <asvegren@gmail.com>
2021-02-23 14:16:13 +0000

    Show longest frames function

--- brofiler/brofiler.lua
+++ brofiler/brofiler.lua
@@ -3,15 +3,22 @@ local M = {}
 
 M.total_time = 0
 M.segments = {}
+M.frame_log = {}
 
 function M.frame_init()
+    
+    
 
     if M.total_time > 0 then
 
+        local num = #M.frame_log+1
+        M.frame_log[num] = {total_time = M.total_time, segments = {}}
+
         print("--BROFILER--")
         print("Tracked segments took a total time of " .. math.ceil(M.total_time * 10000) / 10000 .. "s to execute.")
-        for segment, table in pairs(M.segments) do
-            print(segment .. " took " .. math.ceil(table.total_time * 10000) / 10000 .. "s (" .. math.ceil(table.total_time / M.total_time * 1000) / 10 .. "%), called " .. table.num_calls .. " times")
+        for segment, t in pairs(M.segments) do
+            print(segment .. " took " .. math.ceil(t.total_time * 10000) / 10000 .. "s (" .. math.ceil(t.total_time / M.total_time * 1000) / 10 .. "%), called " .. t.num_calls .. " times")
+            table.insert(M.frame_log[num].segments, {id = segment, time = t.total_time})
         end
         print("--END BROFILER--")
 
@@ -43,6 +50,47 @@ function M.end_segment(segment)
 
     M.total_time = M.total_time + t
 
+end
+
+local function clone(t)
+  local rtn = {}
+  for k, v in pairs(t) do rtn[k] = v end
+  return rtn
+end
+
+local function sort(t, comp)
+    local rtn = clone(t)
+    if comp then
+        if type(comp) == "string" then
+            table.sort(rtn, function(a, b) return a[comp] > b[comp] end)
+        else
+            table.sort(rtn, comp)
+        end
+    else
+        table.sort(rtn)
+    end
+    return rtn
+end
+
+function M.show_longest_frames(num)
+    
+    num = num or 3
+
+    local sorted = sort(M.frame_log, "total_time")
+
+    local suffix_t = {
+        "st", "nd", "rd"
+    }
+    local suffix = nil
+    for i=1,num do
+        if i >= 3 then
+            suffix = suffix_t[3]
+        else
+           suffix = suffix_t[i] 
+        end
+        print(i .. suffix .. " longest frame: ")
+        pprint(sorted[i])
+    end
 end
 
 return M
